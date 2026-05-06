9304d78ba7ad9688ff6befebcafff3f53922aa99
XReykoX <108241786+XReykoX@users.noreply.github.com>
2026-05-05 18:57:34 -0700

    basic web server test

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,79 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESP32Ping.h>
+#include <ESPAsyncWebServer.h> // Web Server library
 #include "secrets.h"
 
-void setup() {
+// initialize the server on port 80
+AsyncWebServer server(80);
+
+// global variables to store the state
+String lastStatus = "Checking...";
+int lastLatency = 0;
+unsigned long lastCheckTime = 0;
+const long interval = 10000; // check every 10 seconds
+
+// Backend Logic
+void checkNetwork()
+{
+  Serial.print("Watchdog checking Google... ");
+  if (Ping.ping("8.8.8.8", 2))
+  {
+    lastStatus = "ONLINE";
+    lastLatency = Ping.averageTime();
+    Serial.println("SUCCESS");
+  }
+  else
+  {
+    lastStatus = "OFFLINE";
+    lastLatency = 0;
+    Serial.println("FAIL");
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
-  
-  // 1. Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  // waiting to connect to wifi
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("\nWiFi Connected. IP: " + WiFi.localIP().toString());
 
-  Serial.println("\nWiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // API + Frontend
+  /*
+  server.on(URL path (/ is root IP), HTTP method (HTTP_GET/POST/DELETE/etc), callback function (what to do when route is hit))
+  */
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+        // basic frontend
+        String html = "<html><body style='font-family:sans-serif; text-align:center;'>";
+        html += "<h1>ESP32 Network Watchdog</h1>";
+        html += "<p>Status: <strong>" + lastStatus + "</strong></p>";
+        html += "<p>Latency: " + String(lastLatency) + "ms</p>";
+        html += "<button onclick='location.reload()'>Refresh</button>";
+        html += "</body></html>";
+        
+        request->send(200, "text/html", html); });
+
+  // start server
+  server.begin();
+
+  // initial check
+  checkNetwork();
 }
 
-void loop() {
-  Serial.print("Pinging Google (8.8.8.8)... ");
-  
-  // 2. The Watchdog Logic
-  bool success = Ping.ping("8.8.8.8", 3); // Pings 3 times
-  
-  if (success) {
-    int avgTime = Ping.averageTime();
-    Serial.printf("SUCCESS Average response time: %d ms\n", avgTime);
-  } else {
-    Serial.println("FAIL Host unreachable.");
+void loop()
+{
+  // non-blocking timer logic
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastCheckTime >= interval)
+  {
+    lastCheckTime = currentMillis;
+    checkNetwork();
   }
-
-  // Wait 10 seconds before checking again
-  delay(10000); 
 }
